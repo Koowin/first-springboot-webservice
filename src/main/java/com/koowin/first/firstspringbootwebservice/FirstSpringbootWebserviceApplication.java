9380393aefdd2befb79b7ef7ebca1169e304b49d
Koowin <56014482+Koowin@users.noreply.github.com>
2022-03-21 10:34:45 +0900

    init

---
+++ src/main/java/com/koowin/first/firstspringbootwebservice/FirstSpringbootWebserviceApplication.java
@@ -0,0 +1,13 @@
+package com.koowin.first.firstspringbootwebservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstSpringbootWebserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FirstSpringbootWebserviceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/koowin/first/firstspringbootwebservice/FirstSpringbootWebserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.koowin.first.firstspringbootwebservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstSpringbootWebserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
