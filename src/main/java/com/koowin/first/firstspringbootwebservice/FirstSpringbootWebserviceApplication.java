5f21cee1bef688ce1858ba2b9f43aef6ccc0a9b1
Koowin <56014482+Koowin@users.noreply.github.com>
2022-03-21 13:56:07 +0900

    helloController

--- src/main/java/com/koowin/first/firstspringbootwebservice/FirstSpringbootWebserviceApplication.java
+++ src/main/java/com/koowin/first/firstspringbootwebservice/FirstSpringbootWebserviceApplication.java
@@ -1,13 +1,14 @@
 package com.koowin.first.firstspringbootwebservice;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class FirstSpringbootWebserviceApplication {
-
+    private static final Logger log = LoggerFactory.getLogger(FirstSpringbootWebserviceApplication.class);
     public static void main(String[] args) {
         SpringApplication.run(FirstSpringbootWebserviceApplication.class, args);
     }
-
 }
